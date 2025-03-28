044a1d571e3023aa9f4dae6ef5bc3e6ca113d123
Feyber Amaya <fdamayas@ufpso.edu.co>
2025-03-28 07:50:24 -0500

    ejemplo array

---
+++ src/ejemArrays.java
@@ -0,0 +1,11 @@
+public class ejemArrays {
+
+    public static void main(String[] args) {
+        int [] numeros = new int [5];
+        numeros [0] = 10;
+        System.out.println(numeros[0]);
+    }
+}
+
+
+
